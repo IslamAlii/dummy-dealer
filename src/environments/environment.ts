9fdb1e9337fdc085c004ffdd52a00f5c6d0b6780
Islam Ali <islamkhtabsolom@yahoo.com>
2023-10-02 06:23:17 +0300

    Fixing some issues and updating the api-url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api.dummydealer.com/api/v1/',
-  apiImageUrl: 'https://api.dummydealer.com/api/v1/images',
+  // apiUrl: 'https://api.dummydealer.com/api/v1/',
+  // apiImageUrl: 'https://api.dummydealer.com/api/v1/images',
+  apiUrl: 'https://eazymoney.vercel.app/api/v1/',
+  apiImageUrl: 'https://eazymoney.vercel.app/api/v1/images',
 };
